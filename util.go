4958c44af21aa085238adc0210c70cfeb55048ea
Ben DeMeyer <ben@bendemeyer.com>
2026-07-06 12:44:11 -0500

    Cleanup old socket files before trying to create a new one

--- util.go
+++ util.go
@@ -6,14 +6,15 @@ import (
 	"os"
 )
 
-
 func GetListener(port string, socket string) (net.Listener, error) {
 	if port != "" {
 		return net.Listen("tcp", fmt.Sprintf("127.0.0.1:%s", port))
 	} else {
+		// Cleanup any old socket file before opening a new one
+		os.Remove(socket)
 		l, err := net.Listen("unix", socket)
 		if err == nil {
-			os.Chmod(socket, 0777)
+			os.Chmod(socket, 0666)
 		}
 		return l, err
 	}
